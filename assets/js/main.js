ff11c8811eac34b6766494d86700934762afd977
Divakar Rajesh <sdivakarrajesh@gmail.com>
2020-09-29 10:42:23 +0530

    Menu styling fix for mobile view

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,23 @@
 $(document).ready(() => {
     var displayed = 0;
+    console.log("ready", displayed)
     $(".fa-bars").click(() => {
-        $("#nav>ul>li").css("display", "block");
+        $("#nav>ul>a>li").css("display", "block");
         $("#nav>ul").css("padding-top", "70px");
         $("#nav").css({ "height": "100vh" });
         console.log('displayed')
         displayed = 1;
         $(".fa-bars").css("display", "none");
         $(".fa-times").css("display", "inline-block");
-
-
     });
 
     $(".fa-times").click(() => {
-        $("#nav>ul>li").css("display", "none");
+        $("#nav>ul>a>li").css("display", "none");
         $("#nav>ul").css("padding-top", "20px");
         $("#nav").css("height", "0px");
         console.log('not displayed')
         displayed = 0;
         $(".fa-times").css("display", "none");
         $(".fa-bars").css("display", "inline-block");
-
     });
-
 })
